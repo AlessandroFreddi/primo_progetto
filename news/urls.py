4f4e1b91a5b0443cd3b3a3e107bb7f8c42d3d470
Alessandro <62058586+thebear06@users.noreply.github.com>
2026-01-19 13:15:21 +0100

    passo 17

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, articoloDetailView, index_news
+from .views import home, articoloDetailView, index_news, listaArticoli
 
 app_name="news"
 urlpatterns = [
     path('news', home, name='home'),
     path("news/articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
-    path('index_news', index_news, name='index_news')
+    path('index_news', index_news, name='index_news'),
+    path('lista_articoli', listaArticoli, name='lista_articoli'),
+    path('lista_articoli/<int:pk>', listaArticoli, name='lista_articoli')
 ]
