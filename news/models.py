853ec66e0fa3b57f68f7a240a998a84ef097b2d8
Alessandro <62058586+thebear06@users.noreply.github.com>
2026-01-19 11:33:15 +0100

    wait a moment

--- news/models.py
+++ news/models.py
@@ -6,9 +6,14 @@ class Giornalista(models.Model):
     """ il modello generico di un giornalista """
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
+    anno_di_nascita = models.DateField(blank=True)
     
     def __str__(self):
         return self.nome + " " + self.cognome
+    
+    class Meta:
+        verbose_name = "Giornalista"
+        verbose_name_plural = "Giornalisti"
 
 
 class Articolo(models.Model):
@@ -16,6 +21,12 @@ class Articolo(models.Model):
     titolo = models.CharField(max_length=100)
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
-    
+    data = models.models.DateField(auto_now=True, blank=True)
+    visualizzazioni = models.IntegerField(default=0,blank=True)
+
     def __str__(self):
         return self.titolo
+    
+    class Meta:
+        verbose_name = "Articolo"
+        verbose_name_plural = "Articoli"
